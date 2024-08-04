0176e66a8d31dbfcd40de6433ef05e363669eb3a
Matveyka1567 <kryktyk@gmail.com>
2024-08-04 12:51:11 +0300

    Update home_work.py

--- home_work.py
+++ home_work.py
@@ -1,14 +1,19 @@
-    # ДЗ № 1
-countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
-capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
-population = ['145_934_462', '331_002_651', '80_345_321', '67_886_011',
-'65_273_511', '1_380_004_385']
-print(f"{capitals[0]} is the capital of {countries[0]}, population equal {population[0].replace('_', '')}")
-print(f"{capitals[1]} is the capital of {countries[1]}, population equal {population[1].replace('_', '')}")
-print(f"{capitals[2]} is the capital of {countries[2]}, population equal {population[2].replace('_', '')}")
+import matplotlib.pyplot as plt
 
+fig = plt.figure()
 
-                    # ДЗ № 2
-def ignore_command(command):
-    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate'] 
-    return any(map(lambda x: x in command, ignore))
+x = [4.0, 4.7, 4.1, 4.4, 5.0, 4.3, 4.6, 4.9, 4.5]
+y = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+plt.title('Мой средний бал в школе с 1 по 9 классы')
+
+plt.plot(x, y)
+
+plt.xlabel('Средний балл')
+plt.ylim(1, 9)
+plt.xlim(4, 5)
+plt.ylabel('Классы')
+
+plt.grid()
+plt.legend()
+
+plt.show()
